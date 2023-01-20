f91171d15e054fa3d7aa78f9feb13d0b49f3d304
Rifat Zahan <26659769+rifatzahan@users.noreply.github.com>
2023-01-19 23:20:27 -0600

    Scatterplot
    Reduction in deaths due to #chatsafe

---
+++ ABM Analysis.R
@@ -0,0 +1,96 @@
+library(foreign)
+library(ggplot2)
+library(hrbrthemes)
+
+# 5000 population
+
+EffectsAxBData <- read.csv("EffectsAxBData.csv", header = T)
+
+pAxB <- ggplot(EffectsAxBData, aes(x = `Chatsafe.SC.reduction`, 
+                                      y = `Fraction.of.deaths.reduced.from.Baseline`, 
+                                      colour = factor(`Influenced.by.Chatsafe`),
+                                      group = factor(`Influenced.by.Chatsafe`)
+)
+) + 
+  geom_line() + 
+  geom_point() + 
+  facet_wrap(~ EffectsAxBData$Scaling.Coefficient, ncol = 2) +
+  theme_bw() +
+  labs(title="",
+       x ="Reduction in Scaling coefficient of probability of re-sharing attempt news due to #chatsafe", 
+       y = "Fraction reduction in the # of deaths from Baseline (no #Chatsafe)"
+  ) +
+  labs(color='') 
+
+
+pAxB
+
+# 1000 populations
+
+EffectsAxBxDData <- read.csv("EffectsAxBxDData.csv", header = T)
+
+pAxBxD <- ggplot(EffectsAxBxDData, aes(x = `Chatsafe.SC.reduction`, 
+                                   y = `Fraction.of.deaths.reduced.from.Baseline`, 
+                                   colour = factor(`Influenced.by.Chatsafe`),
+                                   group = factor(`Influenced.by.Chatsafe`)
+)
+) + 
+  geom_line() + 
+  geom_point() + 
+  facet_wrap(~ EffectsAxBxDData$Scaling.Coefficient, ncol = 2) +
+  theme_bw() +
+  labs(title="",
+       x ="Reduction in Scaling coefficient of probability of re-sharing attempt news due to #chatsafe", 
+       y = "Fraction reduction in the # of deaths from Baseline (no #Chatsafe)"
+  ) +
+  labs(color='') 
+
+
+pAxBxD
+
+
+
+EffectsAxBxCData <- read.csv("EffectsAxBxCData.csv", header = T)
+
+pAxBxC <- ggplot(EffectsAxBxCData, aes(x = `Chatsafe.SC.reduction`, 
+                                       y = `Fraction.of.deaths.reduced.from.Baseline`, 
+                                       colour = factor(`Influenced.by.Chatsafe`),
+                                       group = factor(`Influenced.by.Chatsafe`)
+)
+) + 
+  geom_line() + 
+  geom_point() + 
+  facet_wrap(~ EffectsAxBxCData$Scaling.Coefficient, ncol = 2) +
+  theme_bw() +
+  labs(title="",
+       x ="Reduction in Scaling coefficient of probability of re-sharing attempt news due to #chatsafe", 
+       y = "Fraction reduction in the # of deaths from Baseline (no #Chatsafe)"
+  ) +
+  labs(color='') 
+
+
+pAxBxC
+
+
+
+EffectsAxBxEData <- read.csv("EffectsAxBxEData.csv", header = T)
+
+pAxBxE <- ggplot(EffectsAxBxEData, aes(x = `Chatsafe.SC.reduction`, 
+                                       y = `Fraction.of.deaths.reduced.from.Baseline`, 
+                                       colour = factor(`Influenced.by.Chatsafe`),
+                                       group = factor(`Influenced.by.Chatsafe`)
+)
+) + 
+  geom_line() + 
+  geom_point() + 
+  facet_wrap(~ EffectsAxBxEData$Scaling.Coefficient, ncol = 2) +
+  theme_bw() +
+  labs(title="",
+       x ="Reduction in Scaling coefficient of probability of re-sharing attempt news due to #chatsafe", 
+       y = "Fraction reduction in the # of deaths from Baseline (no #Chatsafe)"
+  ) +
+  labs(color='') 
+
+
+pAxBxE
+
